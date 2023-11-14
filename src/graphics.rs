461d34174c2331ef779312b080ba92f74422adba
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2023-11-14 05:30:43 -0800

    Trying out changing this to try to fix the dark sprites

--- src/graphics.rs
+++ src/graphics.rs
@@ -992,7 +992,7 @@ impl RenderData {
       mip_level_count,
       sample_count: 1,
       dimension: wgpu::TextureDimension::D2,
-      format: wgpu::TextureFormat::Rgba8UnormSrgb,
+      format: wgpu::TextureFormat::Rgba8Unorm,
       usage: wgpu::TextureUsages::TEXTURE_BINDING
         | wgpu::TextureUsages::COPY_DST
         | wgpu::TextureUsages::RENDER_ATTACHMENT,
